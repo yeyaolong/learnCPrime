c4fb7100296808e585510b4d9777decab88b1b03
yeyaolong <470080502@qq.com>
2022-09-24 23:58:48 +0800

    暂存

--- 多态/多态/02 多态的原理剖析.cpp
+++ 多态/多态/02 多态的原理剖析.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 
 // 多态
-class Animal {
+class Animal2 {
 public:
-	void virtual speak() {
+	void speak() {
 		cout << "动物在说话" << endl;
 	}
 };
 
-class Cat: public Animal {
+class Cat2 : public Animal2 {
 public:
 	void speak() {
 		cout << "喵" << endl;
 	}
 };
 
-class Dog : public Animal {
+class Dog2 : public Animal2 {
 public:
 	void speak() {
 		cout << "汪" << endl;
@@ -28,7 +28,7 @@ public:
 // 如果想执行cat.speak() 
 // 这个函数的地址就不能提前确定，需要在运行阶段进行绑定
 // 也就是给父类Animal的成员函数改为虚函数
-void doSpeak(Animal &animal) { // 形参赋值  Animal &animal = cat;
+void doSpeak2(Animal2& animal) {
 	animal.speak();
 }
 
@@ -40,21 +40,28 @@ void doSpeak(Animal &animal) { // 形参赋值  Animal &animal = cat;
 
 // 动态多态使用
 // 父类的指针或者引用 指向子类对象
-void test01() {
-	Cat cat;
+void test02() {
+	Cat2 cat;
 
-	doSpeak(cat);
+	doSpeak2(cat);
 
-	Dog dog;
-	doSpeak(dog);
+	Dog2 dog;
+	doSpeak2(dog);
 
-	Animal animal;
-	doSpeak(animal);
+	Animal2 animal;
+	doSpeak2(animal);
 }
 
-int main1() {
+void test03() {
+	cout << "sizeof animal = " << sizeof(Animal2) << endl; // x86下等于4， x64下等于8
+	// Animal2 没有成员变量，并且不使用virtual时，因为没有成员变量，所以占用内存空间是 1
+	// Animal2 虽然没有成员变量，但是用了virtual，变成多态的成员函数后，占用内存空间增大，变为4或者8字节.
+	// 多出的字节，用来存储 "虚函数指针"
+}
 
-	test01();
+int main() {
+
+	test02();
 
 	system("pause");
 
