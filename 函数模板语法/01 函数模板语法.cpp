960e1e2b380591d74e1464c8741b77cd96584954
叶瑶隆 <yaolong.ye@dbappsecurity.com.cn>
2022-11-03 08:43:00 +0800

    暂存

--- 函数模板语法/01 函数模板语法.cpp
+++ 函数模板语法/01 函数模板语法.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 //  两个整型交换函数
 
-void swatInt(int &a, int &b) {
+void swapInt(int &a, int &b) {
 	int tmp = a;
 	a = b;
 	b = tmp;
@@ -15,18 +15,29 @@ void swatInt(int &a, int &b) {
 
 // 交换两个浮点型函数
 
-void swatDouble(double& a, double& b) {
+void swapDouble(double& a, double& b) {
 	double tmp = a;
 	a = b;
 	b = tmp;
+}
+
+// 函数模板
+template<typename T> // 声明一个模板，告诉编译器后面代码中紧跟着的T不要报错，T是一个通用数据类型
+void mySwap(T& a, T& b) {
+	T temp = a;
+	a = b;
+	b = temp;
 }
 
 void test01() {
 	int a = 10;
 	int b = 20;
 
-	swatInt(a, b);
-
+	//swapInt(a, b);
+	// 自动类型推导
+	//mySwap(a, b);
+	// 显式指定类型
+	mySwap<int>(a, b);
 	cout << "a = " << a << " b = " << b << endl;
 }
 
@@ -35,10 +46,16 @@ void test02() {
 	double a = 10.0;
 	double b = 20.0;
 
-	swatDouble(a, b);
+	// swapDouble(a, b);
+	// 自动类型推导
+	//mySwap(a, b);
+	// 显式指定类型
+	mySwap<double>(a, b);
 
 	cout << "a = " << a << " b = " << b << endl;
 }
+
+
 
 int main() {
 
